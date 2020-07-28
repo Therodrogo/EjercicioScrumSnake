64d1870b4ee8a8253e1ba6e159a02888a698fca9
Rodrigo <rl44610@hotmail.com>
2020-07-28 14:42:36 -0400

    Tablero

--- src/ejercicioscrumsnake/Manzana.java
+++ src/ejercicioscrumsnake/Manzana.java
@@ -5,38 +5,10 @@
  */
 package ejercicioscrumsnake;
 
-import javafx.scene.shape.Rectangle;
-
 /**
  *
  * @author Rodrigo
  */
 public class Manzana {
-    double tamañomanzana= 40;
-    double posx;
-    double posy;
-    Rectangle manzana;
-
-    public double getTamañomanzana() {
-        return tamañomanzana;
-    }
-
-    public void setPosx(double posx) {
-        this.posx = posx;
-    }
-
-    public void setPosy(double posy) {
-        this.posy = posy;
-    }
-
-    public Rectangle getManzana() {
-        manzana.setHeight(getTamañomanzana());
-        manzana.setWidth(getTamañomanzana());
-        manzana.setX(posx);
-        manzana.setY(posy);
-        return manzana;
-    }
-
-   
     
 }
--- src/ejercicioscrumsnake/Serpiente.java
+++ src/ejercicioscrumsnake/Serpiente.java
@@ -1,3 +1,7 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +14,40 @@ package ejercicioscrumsnake;
  * @author Rodrigo
  */
 public class Serpiente {
+
+    public ArrayList<Point> cola = new ArrayList<Point>();
+    public int posicionX = 0;
+    public int posicionY = 0;
+
+    public Serpiente()
+    {
+        cola.add(new Point(posicionX,posicionY));
+    }
     
+    public ArrayList<Point> getCola()
+    {
+        return cola;
+    }
+
+    public void dibujar (Graphics g)
+    {
+        g.setColor(COLOR.BLUE);
+
+    }
+    
+    public mover()
+    {
+            
+    }
+
+    public crecer()
+    {
+        cola.add(new Point());
+    }
+
+    public direcciones()
+    {
+        
+    }
+
 }
